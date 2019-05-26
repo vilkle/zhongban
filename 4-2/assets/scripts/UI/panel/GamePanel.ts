634986f6ed11d756955b79ae093582144eb605ec
machao <525053561@qq.com>
2019-05-26 23:59:26 +0800

    update zhongban

--- 4-2/assets/scripts/UI/panel/GamePanel.ts
+++ 4-2/assets/scripts/UI/panel/GamePanel.ts
@@ -5,9 +5,46 @@ const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class GamePanel extends BaseUI {
+    @property(cc.Node)
+    private amazeBg : cc.Node = null;
+    @property(cc.Mask)
+    private mask : cc.Mask = null;
 
     protected static className = "GamePanel";
+
+    onLoad() {
+        this.addListenerOnAmazeBg();
+    }
+
     start() {
+    }
+
+    addListenerOnAmazeBg() {
+        this.amazeBg.on(cc.Node.EventType.TOUCH_START, function(e){
+            var point = e.touch.getLocation();
+            point = this.amazeBg.convertToNodeSpaceAR(point);
+            this._addCircle(point);
+
+        }.bind(this));
+        this.amazeBg.on(cc.Node.EventType.TOUCH_MOVE, function(e){
+            var point = e.touch.getLocation();
+            point = this.amazeBg.convertToNodeSpaceAR(point);
+            this._addCircle(point);
+        }.bind(this));
+        this.amazeBg.on(cc.Node.EventType.TOUCH_END, function(e){}.bind(this));
+        this.amazeBg.on(cc.Node.EventType.TOUCH_CANCEL, function(e){}.bind(this));
+
+    }
+
+    _addCircle(point : cc.Vec2) {
+        var graphics = this.mask._graphics;
+        console.log("xxxx:",graphics)
+        var color = cc.color(0, 0, 0, 255);
+        graphics.rect(point.x,point.y,50,30)
+        graphics.lineWidth = 2
+        graphics.fillColor = color
+        //graphics.strokeColor = color
+        graphics.fill()
     }
 
     onDestroy() {

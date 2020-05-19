7ecb25b212c2ae5162966ea18084b0a85b6ea053
vilkle <525053561@qq.com>
2020-05-19 16:07:20 +0800

    deleted chidouzi

--- chidouzi/assets/scripts/Data/ConstValue.ts
+++
@@ -1,8 +0,0 @@
-export class ConstValue {
-    public static readonly IS_EDITIONS = false;//是否为发布版本，用于数据上报 及 log输出控制
-    public static readonly IS_TEACHER = false;//是否为教师端版本
-    public static readonly CONFIG_FILE_DIR = "config/";
-    public static readonly PREFAB_UI_DIR = "prefab/ui/panel/";
-    public static readonly AUDIO_DIR = "audio/";
-    public static readonly CoursewareKey = "!8cxp0NFJt0MNXvs*&4nTUL2";//每个课件唯一的key 24位随机字符串 可用随机密码生成器来生成。
-}
